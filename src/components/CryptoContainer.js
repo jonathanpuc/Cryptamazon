e05fa2d25043b40ff844cf04c7c9b03562768ddb
Jonathan Puc <jonathan@mayte.io>
2018-01-18 14:57:51 +1100

    finish

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -1,16 +1,57 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+import FetchCoinData from './../actions/FetchCoinData';
+
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
+
+    componentDidMount() {
+        this.props.FetchCoinData();
+    }
     render() {
+
+        const { crypto } = this.props;
         return (
             <View>
-                <Text>Container</Text>
-            </View>
-        )
+        {crypto.isFetching ? 
+        
+            
+                <Spinner
+                    visible={crypto.isFetching}
+                    textContent={"Loading..."}
+                    textStyle={{color: "#253145"}}
+                    animation="fade"
+                    />
+                
+        :       
+                <ScrollView contentContainerStyle={contentContainer}>
+                {this.renderCoinCards()}
+                </ScrollView>
+
+        }
+        </View>
+    )
+
+    }
+
+    renderCoinCards = () => {
+        const { crypto } = this.props;
+        return crypto.data.map((coin, index) => <CoinCard coin={coin} key={index}/>);
     }
 }
+
+const styles = {
+    contentContainer: {
+        paddingBottom: 100,
+        paddingTop: 55
+    }
+}
+
+const { contentContainer } = styles;
 
 function mapStateToProps(state) {
     return {
@@ -18,4 +59,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(CryptoContainer)
+export default connect(mapStateToProps, {FetchCoinData})(CryptoContainer)
--- src/reducers/CryptoReducer.js
+++ src/reducers/CryptoReducer.js
@@ -1,3 +1,43 @@
-export default function (state = [], action) {
-    return state;
+import {
+    FETCHING_COIN_DATA,
+    FETCHING_COIN_DATA_SUCCESS,
+    FETCHING_COIN_DATA_FAIL 
+} from './../utils/ActionTypes';
+
+const initialState = {
+    isFetching: null,
+    data: [],
+    hasError: false,
+    errorMessage: null
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        
+        case FETCHING_COIN_DATA: 
+        return ({
+            ...state,
+            isFetching: true,
+        });
+
+        case FETCHING_COIN_DATA_SUCCESS:
+        return ({
+            ...state,
+            isFetching: false,
+            data: action.payload,
+        }
+        )
+
+        case FETCHING_COIN_DATA_FAIL:
+        return ({
+            ...state,
+            data: action.payload,
+            hasError: true,
+            errorMessage: action.err
+        })
+
+
+        default: 
+            return state;
+    }
 }
